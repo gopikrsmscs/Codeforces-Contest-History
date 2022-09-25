c1052664a5a0133fc06f7437e13a5028456ee9a1
Pavuluri Gopikrishna <gxp5903@mavs.uta.edu>
2022-09-25 01:41:54 -0500

    Updates

---
+++ precache-manifest.f541414fb015f5be13114d604c66b702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f0c5301b87c3b4d5e31114ec2cc2da",
+    "url": "/codeforces-contests-page/index.html"
+  },
+  {
+    "revision": "f78ee0d3a13140283b9b",
+    "url": "/codeforces-contests-page/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a2c828cb96f8df19aa81",
+    "url": "/codeforces-contests-page/static/css/main.545ecd2c.chunk.css"
+  },
+  {
+    "revision": "f78ee0d3a13140283b9b",
+    "url": "/codeforces-contests-page/static/js/2.94787c6a.chunk.js"
+  },
+  {
+    "revision": "a2c828cb96f8df19aa81",
+    "url": "/codeforces-contests-page/static/js/main.314eae15.chunk.js"
+  },
+  {
+    "revision": "5ceacdff9fbc26d3031a",
+    "url": "/codeforces-contests-page/static/js/runtime~main.0cb3c795.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/codeforces-contests-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
